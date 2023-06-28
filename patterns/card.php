fc93545376f15960a7de1df6853185587539bf81
Thomas Gollenia <thomas@gollenia.at>
2023-06-28 09:31:21 +0200

    Neu

---
+++ patterns/card.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Card
+ * Slug: school/card
+ * Categories: featured
+ * Keywords: Card
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|30"},"blockGap":"0"},"border":{"radius":"0px"}},"className":"msn-card","layout":{"type":"constrained"}} -->
+<div class="wp-block-group msn-card" style="border-radius:0px;padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:image {"id":14,"sizeSlug":"large","linkDestination":"none","style":{"border":{"radius":"0px"}}} -->
+<figure class="wp-block-image size-large has-custom-border"><img src="https://picsum.photos/seed/picsum/800/600" alt="" class="wp-image-14" style="border-radius:0px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"white","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group has-white-background-color has-background msn-card-content" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:heading {"textAlign":"center","level":5,"style":{"typography":{"textTransform":"capitalize"}}} -->
+<h5 class="wp-block-heading has-text-align-center" style="text-transform:capitalize">Text</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Beschreibung</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","backgroundColor":"quinary","textColor":"white"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-white-color has-quinary-background-color has-text-color has-background has-text-align-center wp-element-button">Mehr...</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
