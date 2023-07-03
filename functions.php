a8f0f83652f96b93f8838d3b2af858bea5b5cb2d
Thomas Gollenia <thomas@gollenia.at>
2023-07-03 16:09:52 +0200

    new

--- functions.php
+++ functions.php
@@ -5,6 +5,7 @@ require_once('block.php');
 function msn_add_css() {
 	wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', false, '1.1', 'all');
 	wp_enqueue_style( 'icons', get_template_directory_uri() . '/assets/css/icons.css', false, '1.1', 'all');
+	wp_enqueue_script( 'menu', get_template_directory_uri() . '/assets/menu.js', [], 1.1, true);
 }
 
 function msn_add_admin() {
