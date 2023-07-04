48ac06353e0dade428dd51f6029125bc055ffffb
Thomas Gollenia <thomas@gollenia.at>
2023-07-04 16:54:08 +0200

    card height

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '146e033f16d144edbcca');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'f8ed1713490b128b4cb2');
