8c113625ac4879a06180ff12ca20a414ffc3c3f2
Thomas Gollenia <thomas@gollenia.at>
2023-07-05 09:09:19 +0200

    new stuff

---
+++ patterns/description.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Icon-Beschreibung
+ * Slug: school/description
+ * Categories: featured
+ * Keywords: Description
+ */
+?>
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"msn-description","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group msn-description"><!-- wp:msn/icon {"icon":"phone"} -->
+<div class="wp-block-msn-icon"><i style="font-size:24px" class="material-symbols-outlined">phone</i></div>
+<!-- /wp:msn/icon -->
+
+<!-- wp:paragraph -->
+<p>Text...</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
